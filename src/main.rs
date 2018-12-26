d4418eb63e6c95b415a03a3b1cb0415be06a76bd
adjitt <adjitt@protonmail.com>
2018-12-25 20:37:33 -0700

    minifb hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use minifb::{Key,Window,WindowOptions};
+
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = vec![0;WIDTH*HEIGHT];
+    let mut window = Window::new("Test - ESC to exit",
+    WIDTH,
+    HEIGHT,
+    WindowOptions::default()).unwrap_or_else(|e| panic!("{}",e));
+    while window.is_open() & !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 255;
+        }
+        window.update_with_buffer(&buffer).unwrap();
+    }
 }
